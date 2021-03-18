cf0297777bd4e837c8446f7e3e349e574bce4a7b
Nicholas Bauer <nicholasbauer@outlook.com>
2021-03-18 16:16:49 -0400

    Moved violin shorthand from Plots.jl

--- src/violin.jl
+++ src/violin.jl
@@ -191,4 +191,15 @@ recipetype(::Val{:groupedviolin}, args...) = GroupedViolin(args)
     x, y
 end
 
+"""
+    violin(x,y,z)
+    violin!(x,y,z)
+Make a violin plot.
+# Example
+```julia-repl
+julia> violin(repeat([1,2,3],outer=100),randn(300))
+```
+"""
+@shorthands violin
+
 Plots.@deps groupedviolin violin
